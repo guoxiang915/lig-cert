c826c9012f3fce7663f4f35eda27ad4c9d7e8fdb
guoxiang915 <guoxiang915@hotmail.com>
2020-10-28 22:15:01 -0500

    fix: add missing Redirect import on LoggedRoute

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import loadable from "@loadable/component";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { useAccount } from "/imports/ui/components/hooks/useAccount";
 const AdminLayout = loadable(() => import("/imports/ui/layouts/AdminLayout"));
 const Homepage = loadable(() => import("/imports/ui/homepage"));
