d7cdf2432894f1b173d1b86e3eb14d397931ddf0
guoxiang915 <guoxiang915@hotmail.com>
2020-12-07 21:46:54 -0500

    feat(user modal): update form styles to new design

--- imports/ui/admin/users/UserModal.js
+++ imports/ui/admin/users/UserModal.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import copy from "copy-text-to-clipboard";
 import { Modal } from "/imports/ui/components/Modal";
-import { IconWarning } from "/imports/ui/components/Icons";
+import { IconEdit, IconCopy, IconSync, IconWarning } from "/imports/ui/components/Icons";
 import { authHasError, authErrorMessage } from "/imports/ui/components/Validations";
 import { isEmail } from "/imports/ui/components/Functions";
 import { ListSelector } from "/imports/ui/components/ListSelector";
+import "/imports/ui/stylesheets/form.css";
 
 export default UserModal = ({ isOpen, onClose, user }) => {
 	const [values, setValues] = useState({
@@ -11,10 +13,9 @@ export default UserModal = ({ isOpen, onClose, user }) => {
 		first_name: user.profile.name.first,
 		last_name: user.profile.name.last,
 		roles: user.roles.slice(),
+		new_password: "",
 		errorState: "",
-		loading: false,
-		newPassword: "",
-		resetLoading: false
+		loading: false
 	});
 
 	const handleChange = (event) => {
@@ -22,19 +23,12 @@ export default UserModal = ({ isOpen, onClose, user }) => {
 		setValues({ ...values, [name]: value });
 	};
 
-	const handlePasswordChange = (event) => {
-		event.preventDefault();
-		event.stopPropagation();
-
-		setValues({ ...values, resetLoading: true });
-
+	const handlePasswordChange = () => {
 		Meteor.call("password.update", user._id, (error, result) => {
-			setValues({ ...values, resetLoading: false });
-
 			if (error) {
 				console.warn(error);
 			} else {
-				setValues({ ...values, newPassword: result });
+				setValues({ ...values, new_password: result });
 			}
 		});
 	};
@@ -43,10 +37,10 @@ export default UserModal = ({ isOpen, onClose, user }) => {
 		event.preventDefault();
 		event.stopPropagation();
 
-		if (!values.email) return setValues({ ...values, errorState: "missing-email" });
-		if (!isEmail(values.email)) return setValues({ ...values, errorState: "invalid-email" });
 		if (!values.first_name) return setValues({ ...values, errorState: "missing-fname" });
 		if (!values.last_name) return setValues({ ...values, errorState: "missing-lname" });
+		if (!values.email) return setValues({ ...values, errorState: "missing-email" });
+		if (!isEmail(values.email)) return setValues({ ...values, errorState: "invalid-email" });
 
 		const userData = {
 			email: values.email.toLowerCase(),
@@ -70,46 +64,52 @@ export default UserModal = ({ isOpen, onClose, user }) => {
 	};
 
 	return (
-		<Modal isOpen={isOpen} onClose={onClose}>
-			<form onSubmit={handleSubmit}>
-				<h1>User Editing</h1>
+		<Modal isOpen={isOpen} onClose={onClose} classes="user-modal">
+			<form onSubmit={handleSubmit} className="form-container">
+				<div className="header">
+					<IconEdit />
+					<p>Edit Member</p>
+				</div>
+
+				<div className="two-col">
+					<label className={`${authHasError("first_name", values.errorState) ? "error" : ""}`}>
+						<span>First Name</span>
+						<input type="text" name="first_name" value={values.first_name} onChange={handleChange} placeholder="First name" />
+					</label>
+
+					<label className={`${authHasError("last_name", values.errorState) ? "error" : ""}`}>
+						<span>Last Name</span>
+						<input type="text" name="last_name" value={values.last_name} onChange={handleChange} placeholder="Last name" />
+					</label>
+				</div>
 
 				<label className={`${authHasError("email", values.errorState) ? "error" : ""}`}>
-					Email Address:
+					<span>Email Address</span>
 					<input type="text" name="email" value={values.email} onChange={handleChange} placeholder="Email address" />
 				</label>
 
-				<label className={`${authHasError("first_name", values.errorState) ? "error" : ""}`}>
-					First Name:
-					<input type="text" name="first_name" value={values.first_name} onChange={handleChange} placeholder="First name" />
-				</label>
+				<label>
+					<span>Generate Password</span>
+					<input type="text" value={values.new_password} placeholder="New password displayed here" disabled={true} />
 
-				<label className={`${authHasError("last_name", values.errorState) ? "error" : ""}`}>
-					Last Name:
-					<input type="text" name="last_name" value={values.last_name} onChange={handleChange} placeholder="Last name" />
+					<div className="actions">
+						{values.new_password && <span onClick={() => copy(values.new_password)}><IconCopy /></span>}
+						<span onClick={handlePasswordChange}><IconSync /></span>
+					</div>
 				</label>
 
 				<label>
-					User Roles:
+					<span>User Roles</span>
 					<ListSelector field="roles" options={["admin","course-tcp"]} currentValues={values.roles} saveCallback={handleChange} />
 				</label>
 
-				<hr/>
-				<label>
-					Password reset:
-					<input type="text" value={values.newPassword} placeholder="Generate Password" disabled={true} />
+				{values.errorState && <p className="error-message"><IconWarning />{authErrorMessage(values.errorState)}</p>}
 
-					<button type="button" onClick={handlePasswordChange} disabled={values.resetLoading}>
-						{values.resetLoading ? "Loading..." : "Generate New Password"}
+				<div className="actions">
+					<button type="submit" className="button primary" disabled={values.loading}>
+						{values.loading ? "Loading..." : "Save Changes"}
 					</button>
-				</label>
-				<hr/>
-
-				{values.errorState && <p><IconWarning />{authErrorMessage(values.errorState)}</p>}
-
-				<button type="submit" disabled={values.loading}>
-					{values.loading ? "Loading..." : "Save Changes"}
-				</button>
+				</div>
 			</form>
 		</Modal>
 	);
