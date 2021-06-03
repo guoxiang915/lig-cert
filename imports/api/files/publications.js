60cb9119deb2a8bc0741fcc88b7d523a6508d54f
guoxiang915 <guoxiang915@hotmail.com>
2021-06-03 09:39:19 -0500

    feat(files): add simple pagination and search

--- imports/api/files/publications.js
+++ imports/api/files/publications.js
@@ -1,8 +1,21 @@
 import { Meteor } from "meteor/meteor";
+import { Roles } from "meteor/alanning:roles";
+import { check } from "meteor/check";
 import { FilesCollection } from "/imports/api/files/files";
 
-Meteor.publish("files/list", function () {
+Meteor.publish("files/list", function (filter) {
+	check(filter, {
+		amount_page: Number,
+		query: String
+	});
+
+	if (!this.userId || !Roles.userIsInRole(this.userId, "admin")) { return this.ready(); }
+
+	const searchRegex = new RegExp(filter.query, "i");
+	const search = filter.query ? { name: searchRegex } : {};
+
 	return FilesCollection.find({}, {
+		limit: filter.amount_page + 1,
 		fields: { name: 1, url: 1, createdAt: 1 },
 		sort: { createdAt: -1 }
 	});
--- imports/ui/admin/files/FilesList.js
+++ imports/ui/admin/files/FilesList.js
@@ -3,6 +3,7 @@ import { useTracker } from "meteor/react-meteor-data";
 import { Meteor } from "meteor/meteor";
 import { FilesCollection } from "/imports/api/files/files";
 import { SEO } from "/imports/ui/components/SEO";
+import { SearchInput } from "/imports/ui/components/SearchInput";
 import { _pluck } from "/imports/ui/components/Functions";
 import FileUploader from "/imports/ui/admin/files/FileUploader";
 import FileItem from "/imports/ui/admin/files/FileItem";
@@ -11,18 +12,24 @@ import "/imports/ui/stylesheets/table.css";
 import "/imports/ui/admin/files/styles.css";
 
 export default FilesList = () => {
+	const [filters, setfilters] = useState({ amount_page: 25, query: "" });
 	const [uploader, setUploader] = useState(null);
+	
+	useEffect(() => { window.prerenderReady = true; }, []);
 
 	const { dataLoading, files } = useTracker(() => {
-		const subs = Meteor.subscribe("files/list");
+		const subs = Meteor.subscribe("files/list", filters);
 
 		return {
 			dataLoading: !subs.ready(),
 			files: FilesCollection.find({}, { sort: { createdAt: -1 } }).fetch()
 		};
-	}, []);
+	}, [filters]);
 
-	useEffect(() => { window.prerenderReady = true; }, []);
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setfilters({ ...filters, [name]: value });
+	};
 
 	return (
 		<Fragment>
@@ -33,6 +40,13 @@ export default FilesList = () => {
 				path='admin/files'
 			/>
 
+			<SearchInput
+				field="query"
+				value={filters.query}
+				placeholder="Search file by name"
+				saveCallback={handleChange}
+			/>
+
 			<FileUploader setUploader={setUploader} fileNames={_pluck(files, "name")} />
 
 			{uploader && <FileUploaderItem uploader={uploader} setUploader={setUploader}/>}
@@ -40,6 +54,8 @@ export default FilesList = () => {
 			<div className="table">
 				{dataLoading ? <p>Loading...</p> : files.map(file => <FileItem key={file._id} file={file} />)}
 			</div>
+
+			{files.length > filters.amount_page && <button onClick={() => setfilters({ ...filters, amount_page: filters.amount_page + 25 })}>Load More</button>}
 		</Fragment>
 	);
 };
